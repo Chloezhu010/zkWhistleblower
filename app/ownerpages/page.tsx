dcb8711c537f0ade4de82cac127b71be10ee1c9c
chloezhu010 <chloe.zhu.x@gmail.com>
2024-07-13 14:41:38 +0200

    Updated front end

---
+++ app/ownerpages/page.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import type { NextPage } from "next";
+import { OwnerPageHome } from "~~/components/OwnerPageHome";
+
+
+const OwnerPage: NextPage = () => {
+
+  return (
+    <>
+      <OwnerPageHome />
+    </>
+  );
+};
+
+export default OwnerPage;
