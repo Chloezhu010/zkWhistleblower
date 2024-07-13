7c4cb72b0113674da31a5b82a820828a14650c86
cholezhu010 <chloe.zhu.x@gmail.com>
2024-07-13 17:15:52 +0200

    small fix

--- components/Header.tsx
+++ components/Header.tsx
@@ -115,28 +115,28 @@ export const Header = () => {
 };
 
 
-import Link from "next/link";
-import { Button } from "../ui/button";
+// import Link from "next/link";
+// import { Button } from "../ui/button";
 
-function FlagIcon(props) {
-  return (
-    <svg
-      {...props}
-      xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    >
-      <path d="M4 15s1-1 4-1 5 2 8 2 4-1 4-1V3s-1 1-4 1-5-2-8-2-4 1-4 1z" />
-      <line x1="4" x2="4" y1="22" y2="15" />
-    </svg>
-  )
-}
+// function FlagIcon(props) {
+//   return (
+//     <svg
+//       {...props}
+//       xmlns="http://www.w3.org/2000/svg"
+//       width="24"
+//       height="24"
+//       viewBox="0 0 24 24"
+//       fill="none"
+//       stroke="currentColor"
+//       strokeWidth="2"
+//       strokeLinecap="round"
+//       strokeLinejoin="round"
+//     >
+//       <path d="M4 15s1-1 4-1 5 2 8 2 4-1 4-1V3s-1 1-4 1-5-2-8-2-4 1-4 1z" />
+//       <line x1="4" x2="4" y1="22" y2="15" />
+//     </svg>
+//   )
+// }
 
 // export function Header() {
 //   return (
