a986e4c9fcf4fa540ad4bf6985ed17b04edafdbe
cholezhu010 <chloe.zhu.x@gmail.com>
2024-07-14 08:05:27 +0200

    fix

--- components/OwnerPageHome.tsx
+++ components/OwnerPageHome.tsx
@@ -2,15 +2,12 @@ import * as CONTRACT_ABI from "./abi.json";
 import { DonateList } from "./ownerpage/SideCard/DonationList";
 import { DonateCard } from "./ownerpage/SideCard/Dondation";
 import { ethers } from "ethers";
-import { useRouter } from "next/router";
 import { set } from "nprogress";
 import { useEffect, useState } from "react";
 import { PostCard } from "~~/components/ownerpage/PostCard";
 import { TransactionHistory } from "~~/components/ownerpage/TransactionHistory";
 
 export function OwnerPageHome() {
-  const router = useRouter();
-  const { query } = router;
   const [postInfo, setPostInfo] = useState(null);
 
   useEffect(() => {
@@ -19,7 +16,7 @@ export function OwnerPageHome() {
         window.ethereum as any
       );
 
-      // 创建合约实例
+    
       const contract = new ethers.Contract(
         "0x52A6Edcb61a2d4835347E77aaCb1eA453FBB9e46",
         CONTRACT_ABI,
@@ -27,7 +24,7 @@ export function OwnerPageHome() {
       );
 
       try {
-        const tx = await contract.getPost(query.id);
+        const tx = await contract.getPost(BigInt(1));
         await tx.wait();
         console.log("CID stored in smart contract:", tx);
         setPostInfo(tx);
@@ -36,7 +33,7 @@ export function OwnerPageHome() {
       }
     };
     fetchOwner();
-  }, [query.id]);
+  }, []);
 
   return (
     <div className="flex flex-col min-h-screen bg-muted/40">
@@ -44,7 +41,7 @@ export function OwnerPageHome() {
         <div className="max-w-6xl mx-auto">
           <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
             <div className="col-span-2">
-              <PostCard postInfo={postInfo}/>
+              <PostCard />
               <TransactionHistory />
             </div>
             <div className="space-y-6">
--- components/ScaffoldEthAppWithProviders.tsx
+++ components/ScaffoldEthAppWithProviders.tsx
@@ -1,7 +1,11 @@
 "use client";
 
 import { EthereumWalletConnectors } from "@dynamic-labs/ethereum";
-import { DynamicContextProvider, mergeNetworks } from "@dynamic-labs/sdk-react-core";
+import { EthersExtension } from "@dynamic-labs/ethers-v5";
+import {
+  DynamicContextProvider,
+  mergeNetworks,
+} from "@dynamic-labs/sdk-react-core";
 import { DynamicWagmiConnector } from "@dynamic-labs/wagmi-connector";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { useTheme } from "next-themes";
@@ -12,11 +16,8 @@ import { Header } from "~~/components/Header";
 import { customEvmNetworks } from "~~/lib/networks";
 import scaffoldConfig from "~~/scaffold.config";
 import { wagmiConfig } from "~~/services/web3/wagmiConfig";
-import { EthersExtension } from "@dynamic-labs/ethers-v5";
-
 
 const ScaffoldEthApp = ({ children }: { children: React.ReactNode }) => {
-
   return (
     <>
       <div className="flex flex-col min-h-screen">
@@ -38,7 +39,7 @@ export const queryClient = new QueryClient({
 });
 
 const evmNetworks = [
-  ...scaffoldConfig.targetNetworks.map(chain => ({
+  ...scaffoldConfig.targetNetworks.map((chain) => ({
     blockExplorerUrls: chain.blockExplorers
       ? Object.values(chain.blockExplorers as any).map(({ url }: any) => url)
       : [],
@@ -47,29 +48,33 @@ const evmNetworks = [
     rpcUrls: Object.values(chain.rpcUrls).map(({ http }) => http[0]),
     nativeCurrency: chain.nativeCurrency,
     networkId: chain.id,
+    iconUrls: [], // Add the iconUrls property here
   })),
   ...customEvmNetworks,
 ];
 
-export const ScaffoldEthAppWithProviders = ({ children }: { children: React.ReactNode }) => {
+export const ScaffoldEthAppWithProviders = ({
+  children,
+}: {
+  children: React.ReactNode;
+}) => {
   const { resolvedTheme } = useTheme();
 
   return (
     <DynamicContextProvider
       theme={resolvedTheme === "dark" ? "dark" : "light"}
       settings={{
-        environmentId: '1e041865-5427-46fa-a2b0-410ea4425e2a',
+        environmentId: "1e041865-5427-46fa-a2b0-410ea4425e2a",
         walletConnectors: [EthereumWalletConnectors],
         walletConnectorExtensions: [EthersExtension],
         overrides: {
-          evmNetworks: networks => mergeNetworks(evmNetworks, networks),
+          evmNetworks: (networks) => mergeNetworks(evmNetworks, networks),
         },
       }}
     >
       <WagmiProvider config={wagmiConfig}>
         <QueryClientProvider client={queryClient}>
           <DynamicWagmiConnector>
-
             <ScaffoldEthApp>{children}</ScaffoldEthApp>
           </DynamicWagmiConnector>
         </QueryClientProvider>
